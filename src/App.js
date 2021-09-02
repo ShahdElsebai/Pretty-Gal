71d87f82e9a26f9cdaf1f2c888d5c1568787a73a
shahd <shahdhosny58@gmail.com>
2021-09-02 04:21:45 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// import Header from "./Components/header"
+import Header from "@Components/header"
+
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <RalewayThinText>Raleway Thin Text</RalewayThinText>
+      <RalewayExtraLightText>Raleway Extra Light Text</RalewayExtraLightText>
+      <RalewayLightText>Raleway Light Text</RalewayLightText>
+      <RalewayRegularText>Raleway Regular Text</RalewayRegularText>
+      <RalewayMediumText>Raleway Medium Text</RalewayMediumText>
     </div>
-  );
+  )
 }
 
-export default App;
+const RalewayThinText = styled.p`
+  font-family: Raleway, serif;
+  font-weight: 100;
+`
+
+const RalewayExtraLightText = styled.p`
+  font-family: Raleway, serif;
+  font-weight: 200;
+`
+
+const RalewayLightText = styled.p`
+  font-family: Raleway, serif;
+  font-weight: 300;
+`
+
+const RalewayRegularText = styled.p`
+  font-family: Raleway, serif;
+  font-weight: 400;
+`
+
+const RalewayMediumText = styled.p`
+  font-family: Raleway, serif;
+  font-weight: 500;
+`
+
+export default App
---
+++ src/Components/header.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+const Header = () => (
+  <StyledHeader>Pretty Gal (Raleway)</StyledHeader>
+)
+
+const StyledHeader = styled.p`
+  font-family: Raleway, serif;
+`
+
+export default Header
