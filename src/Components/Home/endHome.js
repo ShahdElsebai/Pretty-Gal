0966b9ba149a2ba46e87adc8b254ff81734450f8
shahd <shahdhosny58@gmail.com>
2021-09-05 18:25:15 +0200

    Home Cards

--- src/Components/Home/endHome.js
+++ src/Components/Home/endHome.js
@@ -1,18 +1,33 @@
 import styled from "styled-components";
 const EndHome = () => (
+    <div>
 
-   <StyledContainer>
-        <div>
-         <StyledHeader1>YEAR ROUND</StyledHeader1>
-         <StyledLine/>
-         <StyledHeader2>Must Have Items</StyledHeader2>
-        </div>
-   </StyledContainer>
+       <StyledContainer>
+            <div>
+             <StyledHeader1>YEAR ROUND</StyledHeader1>
+             <StyledLine/>
+             <StyledHeader2>Must Have Items</StyledHeader2>
+            </div>
+       </StyledContainer>
+       <StyledContainer>
+            <StyledProductCard>
+                <div>
+                </div>
+            </StyledProductCard>
+            <StyledProductCard/>
+            <StyledProductCard>
+                <div>
+                </div>
+            </StyledProductCard>
+       </StyledContainer>
+
+    </div>
 
 
 )
 
 export const StyledContainer = styled.div`
+  
   display: flex;
   justify-content:center;
   align-items: center;
@@ -22,14 +37,13 @@ export const StyledContainer = styled.div`
 `
 
 export const StyledHeader1 = styled.h1`
+  
    font-family: Helvetica-Neue-Font, serif;
    font-weight: 700;
    font-size: 2.5rem;
    letter-spacing: 10px;
    margin-top: 70px;
-   
-
-
+ 
 `
 export const StyledHeader2 = styled.h1`
 
@@ -38,16 +52,31 @@ export const StyledHeader2 = styled.h1`
    font-size: 1.2rem;
    margin-top: 50px;
    margin-left: 50px;
+   margin-bottom: 70px;
    letter-spacing: 8px;
    
-
 `
 export const StyledLine = styled.hr`
-  border-top: 1px solid black;
+  
+   border-top: 1px solid black;
    margin-top: 50px;
    width: 40px;
   
 `
 export const StyledProductCard = styled.div`
+  
+  background-color: rgb(181, 178, 178);
+  width: 350px;
+  height: 470px;
+  margin: 10px;
+  margin-bottom: 40px;
+  & div {
+    background: #514a4a;
+    width: 100%;
+    height: 15%;
+    top: 85%;
+    position: relative;
+  }
+  
 `
 export default EndHome;
