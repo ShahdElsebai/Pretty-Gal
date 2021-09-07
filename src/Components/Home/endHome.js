53d61d87c25eccac104ed92490ac77f747983a11
shahd <shahdhosny58@gmail.com>
2021-09-07 23:45:34 +0200

    home cards add labels, animation and change text on hover

--- src/Components/Home/endHome.js
+++ src/Components/Home/endHome.js
@@ -1,4 +1,7 @@
-import styled from "styled-components";
+import styled, {css} from "styled-components";
+import skirtImg from "../../assets/imgs/skirt.jpg"
+import glassesImg from "../../assets/imgs/glasses.jpg"
+import bagImg from "../../assets/imgs/bag.jpg"
 const EndHome = () => (
     <div>
 
@@ -10,15 +13,17 @@ const EndHome = () => (
             </div>
        </StyledContainer>
        <StyledContainer>
-            <StyledProductCard>
+            <StyledProductCard backgroundImg = {skirtImg} flage ={true}>
                 <div>
-                    <p>MIDI PLEATED SKIRT</p>
+                    <p data-hover = "View Details">MIDI PLEATED SKIRT</p>
                 </div>
             </StyledProductCard>
-            <StyledProductCard/>
-            <StyledProductCard>
+            <StyledProductCard backgroundImg = {bagImg} flage ={false}>
+                <label>SALE</label>
+            </StyledProductCard>
+            <StyledProductCard backgroundImg = {glassesImg} flage ={true}>
                 <div>
-                    <p>VINTAGE FRAME EYEGLASSES</p>
+                    <p data-hover = "View Details">VINTAGE FRAME EYEGLASSES</p>
                 </div>
             </StyledProductCard>
        </StyledContainer>
@@ -54,7 +59,7 @@ export const StyledHeader2 = styled.h1`
    font-size: 1.2rem;
    margin-top: 50px;
    margin-left: 50px;
-   margin-bottom: 70px;
+   margin-bottom: 50px;
    letter-spacing: 8px;
    
 `
@@ -68,10 +73,31 @@ export const StyledLine = styled.hr`
 export const StyledProductCard = styled.div`
 
   background-color: #c6c5c5;
+  background-image: url(${props => props.backgroundImg});
   width: 300px;
   height: 400px;
   margin: 10px;
   margin-bottom: 40px;
+  //transition: width 2s;
+
+
+  &:hover {
+    // 
+    ${props => props.flage && css`
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+    `} 
+    & p{
+      font-size: 0;
+    }
+    & p:after{
+      content: attr(data-hover);
+      font-family: Raleway, serif;
+      font-weight: 400;
+      font-size: 1rem;
+    }
+  }
 
   & div {
     background: #3a3535;
@@ -89,9 +115,26 @@ export const StyledProductCard = styled.div`
     font-weight: 300;
     font-size: 1rem;
     color: white;
+  }
+ 
+  & label {
+    position: relative;
+    font-size: 3.5rem;
+    font-family: Helvetica-Neue-Font, serif;
+    font-weight: 400;
+    letter-spacing: 8px;
+    margin-left: 70px;
+    top: 70px;
+    color: #3d3a3a;
+
+    &:hover {
+      color: #878181;
+
+    }
 
   }
-
-
+  
 `
+
+
 export default EndHome;
